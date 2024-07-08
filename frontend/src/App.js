bcef4f78702aa052fe535d899c0f239acf1a25b8
Prasad Patil <74979759+PrasadByte@users.noreply.github.com>
2024-07-08 16:01:19 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Create from './components/Create';
+import Header from './components/Header';
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Contact from "./component/contact";
+//import Create from './components/Create';
+
+function App() {
+  return (
+   <>
+   <Header></Header>
+   <Create />
+   <p>hi my name is prasad patil from jalgaom maharashtra,
+   i have completed my bechelore degree in electrical Engineering from the savitribait phule pune univercity along with degree i have completed python certification and Exel and Data anyalytic now im persuing the cdac for the gaining the khowldged of webDevelopment and backend development 
+   </p>
+   
+   
+   
+   </>
+  );
+}
+export default App;
